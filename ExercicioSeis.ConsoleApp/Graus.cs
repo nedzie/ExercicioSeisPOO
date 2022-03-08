77228bd8f8245de3a07a17b370410e98b1393588
nedzie <marcos-elivelton@hotmail.com>
2022-03-08 15:46:40 -0300

    Add project files.

---
+++ ExercicioSeis.ConsoleApp/Graus.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioSeis.ConsoleApp
+{
+    internal class Graus
+    {
+        public double celsius;
+        public double fahrenheit;
+        public double ConverterCelsiusFahrenheit(double fahrenheit)
+        {
+            fahrenheit = (celsius * 1.8) + 32;
+            return fahrenheit;
+        }
+    }
+}
---
+++ ExercicioSeis.ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExercicioSeis.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graus celsius = new Graus();
+            Console.Write("Informa a temperatura em ºC: \n> ");
+            celsius.celsius = double.Parse(Console.ReadLine());
+            celsius.fahrenheit = celsius.ConverterCelsiusFahrenheit(celsius.fahrenheit);
+            Console.Clear();
+            Console.WriteLine(celsius.celsius + "°F, convertidos ficam " + celsius.fahrenheit.ToString("#.##") + "°C.");
+            Console.ReadKey();
+        }
+    }
+}
